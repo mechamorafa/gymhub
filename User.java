f026868bb0c6b09dab32e05d94d01761730add10
Rafael Ramos <rafaelramos@live.com>
2021-08-09 17:29:15 -0300

    Update User.java

--- User.java
+++ User.java
@@ -1,1 +1,13 @@
 
+public class User {
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private int idade;
+    private String box;
+    
+    
+    
+    
+    
+}
