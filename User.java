48a0279ebac300f8dea1dd0d8945ad1416012e63
Rafael Ramos <rafaelramos@live.com>
2021-08-09 17:32:46 -0300

    Update User.java

--- User.java
+++ User.java
@@ -6,4 +6,60 @@ public class User {
     private String box;
     private int plan;
 
+    public User(String nome, String sobrenome, String email, int idade, String box, int plan) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.idade = idade;
+        this.box = box;
+        this.plan = plan;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getBox() {
+        return box;
+    }
+
+    public void setBox(String box) {
+        this.box = box;
+    }
+
+    public int getPlan() {
+        return plan;
+    }
+
+    public void setPlan(int plan) {
+        this.plan = plan;
+    }
 }
